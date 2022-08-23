d78f48b705c2d6a68b06f70a356047fdfaba25b4
Do Hoang Khoi <92308124+KhoiDOO@users.noreply.github.com>
2022-08-23 18:11:21 +0700

    Update Logistic Regression

--- SmartMachineLearning/Training/Classification/linear_poly.py
+++ SmartMachineLearning/Training/Classification/linear_poly.py
@@ -1,8 +1,8 @@
 from SmartMachineLearning.Training.Monitor import SmartTraining
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from datetime import datetime
 
-class SmartRandomForest(SmartTraining):
+class SmartLogisticRegression(SmartTraining):
     def __init__(self, 
                  number_class=2, 
                  number_label=1, 
@@ -14,11 +14,10 @@ class SmartRandomForest(SmartTraining):
                  test_size=0.33, 
                  random_state=42,
                  params_dict = {
-                     "n_estimators" : [50, 100, 150, 200],
-                     "criterion" : ["gini", "entropy", "log_loss"],
-                     "max_features" : ["sqrt", "log2", None],
-                     "bootstrap" : [True, False],
-                     "class_weight" : ["balanced", "balanced_subsample", None]
+                     "penalty" : ["l2"],
+                     "fit_intercept" : [True, False],
+                     "class_weight" : ["balanced", None],
+                     "solver" : ["newton-cg", "lbfgs", "liblinear", "sag", "saga"]
                      }):
         super().__init__(number_class, 
                          number_label, 
@@ -31,11 +30,10 @@ class SmartRandomForest(SmartTraining):
                          random_state)
         self.params_dict = params_dict
     
-    def training(self, n_estimators, 
-                 criterion, 
-                 max_features, 
-                 bootstrap, 
+    def training(self, penalty, 
+                 fit_intercept, 
                  class_weight, 
+                 solver,
                  njob):
         fold_index = 0
         for train_index, test_index in self.kf.split(self.X_data):
@@ -50,40 +48,33 @@ class SmartRandomForest(SmartTraining):
             # Training
             print("\t\tTraining : {}".format(fold_index), end = " -- ")
             print("Start: {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")), end=" --- ")
-            model_RF = RandomForestClassifier(n_estimators = n_estimators, 
-                                              criterion = criterion, 
-                                              max_features = max_features, 
-                                              bootstrap = bootstrap,
-                                              class_weight = class_weight, 
-                                              n_jobs = njob)
-            model_RF.fit(X_train,y_train)
+            model = LogisticRegression(penalty = penalty,
+                                         fit_intercept = fit_intercept,
+                                         class_weight = class_weight,
+                                         solver = solver,
+                                          n_jobs = njob)
+            model.fit(X_train,y_train)
             print("End: {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
         
             # Testing
             print("\t\tValidation: {}".format(fold_index), end = " -- ")
             print("Start: {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")), end=" --- ")
-            y_pred = model_RF.predict(X_test)
+            y_pred = model.predict(X_test)
             print("End: {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
         
             # Evaluation
-            base_params = {"n_estimators" : n_estimators, 
-                           "criterion" : criterion, 
-                           "max_features" : max_features, 
-                           "bootstrap" : bootstrap, 
-                           "class_weight" : class_weight}
+            base_params = {"penalty" : penalty, 
+                           "fit_intercept" : fit_intercept, 
+                           "class_weight" : class_weight, 
+                           "solver" : solver}
             self.add_results(y_true = y_test, y_pred = y_pred, params_dict = base_params)
             fold_index += 1
         
     def smartfit(self):
         count = 0
-        for x in self.params_dict["n_estimators"]:
-            for i in self.params_dict["criterion"]:
-                for j in self.params_dict["max_features"]:
-                    for k in self.params_dict["bootstrap"]:
-                        for l in self.params_dict["class_weight"]:
+        for x in self.params_dict["penalty"]:
+            for i in self.params_dict["fit_intercept"]:
+                for j in self.params_dict["class_weight"]:
+                    for k in self.params_dict["solver"]:
                             print("Traning Case: {}".format(count))
-                            self.training(x, i, j, k, l, 5)  
-    
-    
-    
-    
+                            self.training(x, i, j, k, 5)
